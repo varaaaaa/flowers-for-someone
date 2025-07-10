852924344c5dcf79c58f4af33529464516575ca7
valinsa <yourgirl794@gmail.com>
2025-07-11 02:24:42 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Birthday Yumnaa!!').split('')
+    const titles = ('Happy Birthday Yumnaa!').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
